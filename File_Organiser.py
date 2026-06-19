2d68bb253a202b032347e7989f2baa8bc53b6b98
Doctor alfa <faiquealam@gmail.com>
2026-06-19 16:38:38 +0530

    Add persistent JSON mapping for unknown extensions

--- File_Organiser.py
+++ File_Organiser.py
@@ -1,5 +1,14 @@
 import os
 import shutil
+import json
+import pyttsx3
+engine = pyttsx3.init()
+
+
+with open("mapping_rules.json", "r") as file:
+   mapping_rules = json.load(file)
+
+print(mapping_rules)   
 
 def organize_folder(folder_path):
    print(folder_path)
@@ -31,6 +40,12 @@ def organize_folder(folder_path):
       for ext in extensions:
          extension_map[ext] = folder_path_created
 
+   for ext, folder_name in mapping_rules.items():
+         folder_path_created = os.path.join(folder_path, folder_name)
+         os.makedirs(folder_path_created, exist_ok=True)
+
+         extension_map[ext] = folder_path_created
+
    for item in items:
       print(item)
 
@@ -48,6 +63,25 @@ def organize_folder(folder_path):
          destination_folder = extension_map.get(extension)
          print("Destination Folder:", destination_folder)
 
+         if destination_folder is None:
+
+            print("Unknown extension detected Sir:", extension)
+
+            engine.say(f"Unknown extension detected, Sir. {extension}")
+            engine.runAndWait()
+
+            folder_name = input("Enter folder name for this extension: ")
+
+            destination_folder = os.path.join(folder_path, folder_name)
+            os.makedirs(destination_folder, exist_ok=True)
+
+            mapping_rules[extension] = folder_name
+            
+            with open("mapping_rules.json", "w") as file:
+               json.dump(mapping_rules, file, indent=4)
+               
+            extension_map[extension] = destination_folder
+
          if destination_folder:
 
             destination_path = os.path.join(destination_folder, item)
@@ -59,7 +93,7 @@ def organize_folder(folder_path):
             else:
                print(item, "already exists. Skipping...")   
 
-
+organize_folder(r"C:\Users\Faique\Downloads")
    
    
 
